30f32ba74fe7f0720af848845f28817150a5c3e9
neil <yangchen211@126.com>
2021-04-08 23:51:19 +0800

    第一次提交

---
+++ test_sample.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+"""
+@author: yangwei
+@contact: yangchen211@126.com
+@time: 2021/4/8 11:48 下午
+@file: test_sample.py
+@desc: 
+"""
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 5
