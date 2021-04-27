4d4c20220104f2b87663a8da5c0deb746b2bbd05
neil <yangchen211@126.com>
2021-04-27 23:42:33 +0800

    appium PO

---
+++ test_appium/page/contactlist_page.py
@@ -0,0 +1,48 @@
+# encoding: utf-8
+"""
+@author: yangwei
+@contact: yangchen211@126.com
+@time: 2021/4/24 4:20 下午
+@file: contactlist_page.py
+@desc: 
+"""
+from appium.webdriver.common.mobileby import MobileBy
+from appium.webdriver.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
+
+from test_appium.page.add_member_page import AddMemberPage
+
+
+class ContactListPage:
+    def __init__(self,driver:WebDriver):
+        self.driver = driver
+
+    def swipe_find(self ,text ,num = 3):
+
+        self.driver.implicitly_wait(1)
+        for i in range(0 ,num):
+
+            try:
+                element = self.driver.find_element(MobileBy.XPATH, f"//*[@text='{text}']")
+                self.driver.implicitly_wait(5)
+                return element
+            except NoSuchElementException:
+                print("未找到，滑动")
+                size = self.driver.get_window_size()
+                width = size['width']
+                height = size['height']
+
+                start_x = width / 2
+                start_y = height * 0.8
+                end_x = start_x
+                end_y = height * 0.3
+                duration = 2000
+                self.driver.swipe(start_x, start_y, end_x, end_y, duration)
+
+            if i == num -1:
+                self.driver.implicitly_wait(5)
+                raise NoSuchElementException(f"找了{i}次，未找到")
+
+    def goto_addmember(self):
+        self.swipe_find('添加成员').click()
+        return AddMemberPage(self.driver)
