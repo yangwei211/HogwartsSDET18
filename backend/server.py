cdd5cef5544b96718497f59c82d25f72501b85b0
neil <yangchen211@126.com>
2021-08-06 09:38:47 +0800

    前端第二次练习

--- backend/server.py
+++ backend/server.py
@@ -17,13 +17,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 api = Api(app)
 # 解决跨域
-CORS(app,supports_credentials=True)
+CORS(app, supports_credentials=True)
 # 使用MySQL进行连接
-username='root'
-pwd='admin'
-ip='192.168.31.94'
-port='3306'
-database='test_ck18'
+username = 'root'
+pwd = 'admin'
+ip = '192.168.31.94'
+port = '3306'
+database = 'test_ck18'
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     f'mysql+pymysql://{username}:{pwd}@{ip}:{port}/{database}?charset=utf8'
 # 解决数据库warning问题
@@ -41,7 +41,8 @@ class Testcase(db.Model):
         返回一个标准的Python结构体
         :return:
         """
-        return {'id':self.id, 'nodeid':self.nodeid,'remark':self.remark}
+        return {'id': self.id, 'nodeid': self.nodeid, 'remark': self.remark}
+
 
 # 类代表是哪个接口资源，每个方法代表对此资源的操作，比如get/post
 # 在类服务中继承resouce,表示使用flask-restful
@@ -49,6 +50,7 @@ class TestCaseService(Resource):
     """
     测试用例服务
     """
+
     # 方法名，对应app.route中的methods
     def get(self):
         """
@@ -70,7 +72,7 @@ class TestCaseService(Resource):
             # data =[{"id":i.id, "nodeid":i.nodeid, "remark":i.remark} for i in case_data]
             data = [i.as_dict() for i in case_data]
             # app.logger.info(data)
-        return {"error":0,"msg":{"data":data}}
+        return {"error": 0, "msg": {"data": data}}
 
     def post(self):
         # 增加一条用例
@@ -79,39 +81,39 @@ class TestCaseService(Resource):
         # 从接口中拿到的字典数据进行解包，使用关键字传参传入Testcase
         testcase = Testcase(**case_data)
         # 如果数据字段存在列表，需要做一次转换
-        testcase.nodeid = json.dumps(request.json.get('nodeid'))
+        # 每次如果dumps，那么字符串会添加""
+        # testcase.nodeid = json.dumps(request.json.get('nodeid'))
         db.session.add(testcase)
         db.session.commit()
-        return {"error":0,"message":"post success"}
+        return {"error": 0, "message": "post success"}
 
     def put(self):
         # 修改
         # 获取被修改的接口信息
         case_id = request.json.get("id")
         # 通过id找到被修改的接口信息然后做修改操作
-        case = Testcase.query.filter_by(id=case_id).\
+        case = Testcase.query.filter_by(id=case_id). \
             update(request.json)
+        # 修改之后commit到数据库
         db.session.commit()
         app.logger.info(f'数据已修改，id{case}被修改为{request.json}')
-        return {"error":0,"msg":{"id": case}}
+        return {"error": 0, "msg": {"id": case}}
 
     def delete(self):
         case_id = request.args.get('id')
         if not case_id:
-            return {'error':40001,"msg":"delete case_id can't be null"}
+            return {'error': 40001, "msg": "delete case_id can't be null"}
         # 返回一个主键
         case = Testcase.query.filter_by(id=case_id).delete()
         db.session.commit()
         app.logger.info(case)
 
-        return {"error":0,"msg":{"id": case}}
-
-
+        return {"error": 0, "msg": {"id": case}}
 
 
 if __name__ == '__main__':
     # 把服务添加到app flask中
     # 第一个参数是添加的接口服务，第二个参数，是指定对应接口服务使用的路由
     # db.create_all()
-    api.add_resource(TestCaseService,"/testcase")
+    api.add_resource(TestCaseService, "/testcase")
     app.run(debug=True)
